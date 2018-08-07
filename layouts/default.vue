fcdc035f57a37bd44fd5cc7b524b7851ebec1ff7
Kaplaugher <kaplaugher94@gmail.com>
2018-08-06 20:45:37 -0400

    init

---
+++ layouts/default.vue
@@ -0,0 +1,53 @@
+<template>
+  <v-app >
+    <v-navigation-drawer
+      v-model="drawer"
+      fixed
+      app
+    >
+      <v-list>
+        <v-list-tile
+          router
+          :to="item.to"
+          :key="i"
+          v-for="(item, i) in items"
+          exact
+        >
+          <v-list-tile-content>
+            <v-list-tile-title v-text="item.title"></v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+    <v-toolbar fixed app>
+      <v-toolbar-side-icon @click="drawer = !drawer"></v-toolbar-side-icon>
+
+    </v-toolbar>
+    <v-content>
+      <v-container>
+        <nuxt />
+      </v-container>
+    </v-content>
+    <v-footer :fixed="fixed" app>
+      <span>&copy; 2017</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      drawer: false,
+      items: [
+        { icon: "apps", title: "Welcome", to: "/" },
+        { icon: "bubble_chart", title: "Admin", to: "/admin" },
+        { icon: "bubble_chart", title: "About", to: "/about" },
+        { icon: "bubble_chart", title: "Products", to: "/products" }
+      ],
+      title: "Vuetify.js",
+      fixed: true
+    };
+  }
+};
+</script>
---
+++ pages/admin.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-container>
+    <v-layout row wrap>
+      <v-flex xs12>
+        <h1>Admin Section</h1>
+
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
